67f88f994d5d6a61e2ca0b9b3022c70e040c1302
huythanthanh <maxtistgaming0401@gmail.com>
2019-09-30 23:42:07 +0700

    update

--- session9/huy8.py
+++ session9/huy8.py
@@ -7,12 +7,20 @@ for i in listword:
     print (count,".",i.upper())
 
 dem = 0
-huy = [150300,247100,333300,266800,420900,318000]
+huy = ['150,300','247,100','333,300','266,800','420,900','318,000']
+#Cái này là em lập danh sách để tìm max nhé =)))
+huy1 = [150300,247100,333300,266800,420900,318000]
+
 print ("Danh sách dân số từng quận theo thứ tự:")  
 for i in huy:
     dem += 1
     print (dem, '.', i)  
 #Tìm dân số max min
-print ("Dân số cao nhất trong các quận là:", max(huy))
-if max(huy) == 420900:
-    print ("Quận có dân số cao nhất là DD")
+print ("Dân số cao nhất trong các quận là:", max(huy1))
+print ("Dân số thấp nhất trong các quận là:", min(huy1))
+if max(huy1) == 420900:
+    print ("Quận có dân số cao nhất là DD, rộng 9,96 km2")
+elif min(huy1) == 150300:
+    print ("Quận có dân số thấp nhất là ST, rộng 117,3 km2")
+# Đề ko cho input output em khó tưởng tượng đc cách làm :((
+# Em lười nên tích hợp cả part 5 vào 1 cái nhé anh :>
