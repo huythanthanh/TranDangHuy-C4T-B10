8e1f92d463af449be54e5df5ba5f79c6a8c275e6
huythanthanh <maxtistgaming0401@gmail.com>
2019-11-06 18:47:01 +0700

    update

--- web_session2/JS_DOM-Find.js
+++ web_session2/JS_DOM-Find.js
@@ -1,2 +1,11 @@
 var el = document.getElementById("song_container");
 console.log(el)
+
+var delbutton = document.getElementById("delbutton");
+console.log(delbutton)
+
+var delbutton2 = document.getElementById("delbutton2");
+console.log(delbutton2)
+
+var delbutton3 = document.getElementById("delbutton3");
+console.log(delbutton3)
