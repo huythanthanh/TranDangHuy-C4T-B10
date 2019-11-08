9694aa5e5d157f241b5a3c42ca74c656ebd2789b
huythanthanh <maxtistgaming0401@gmail.com>
2019-11-08 22:59:04 +0700

    update

--- web_session2/JS_DOM-Find.js
+++ web_session2/JS_DOM-Find.js
@@ -1,5 +1,10 @@
 var el = document.getElementById("song_container");
-console.log(el)
+el.addEventListener('click', function(g) {
+    var element = g.target;
+    var eldiv = element.parentNode;
+    console.log(eldiv);
+})
+
 
 var delbutton = document.getElementsByClassName("del_button");
 
@@ -12,3 +17,23 @@ for ( var i = 0; i < delbutton.length; i++) {
     });
 }
 
+var editbutton = document.getElementsByClassName('edit_button');
+
+for (var a = 0; a < editbutton.length; a++) {
+    var editbutton2 = editbutton [a];
+    editbutton2.addEventListener('click', function(f) {
+        var edit = f.target;
+        console.log(f.path[1].attributes[1].value)
+    });
+}
+
+var morebutton = document.getElementsByClassName('more_button');
+for (var m = 0; m < morebutton.length; m++) {
+    var morebutton2 = morebutton [m];
+    morebutton2.addEventListener('click', function(k) {
+        var more = k.target;
+        console.log(k.path[1].attributes[1].value);
+        console.log(k.path[1].attributes[2].value);
+        console.log(k.path[1].attributes[3].value)
+    })
+}
